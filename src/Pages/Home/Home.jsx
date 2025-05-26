fc5ffc8b77198db4f81bcbed5c5b6cbb32c0997e
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-26 15:18:05 +0600

    added and realod

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Shared/Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is HOme</h1>
+            <Banner />
         </div>
     );
 };
