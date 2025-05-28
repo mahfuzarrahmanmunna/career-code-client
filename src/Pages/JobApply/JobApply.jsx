e69e7301ebe6c9432794ede6a4017308a22a9785
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-28 11:34:13 +0600

    work at apply jobs

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import useAuth from '../../Hooks/useAuth';
 
 const JobApply = () => {
+    const { id: jobId } = useParams();
+    const { user } = useAuth()
+    console.log(user, jobId);
+
+    const handleApply = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const linkedIn = form.linkedIn.value;
+        const gitHub = form.gitHub.value;
+        const resume = form.resume.value;
+
+        const applicationData = {
+            jobId,
+            userId: user?.uid,
+            linkedIn,
+            gitHub,
+            resume
+        };
+
+        console.log(applicationData);
+        // Here you can send the applicationData to your server or API
+    }
     return (
         <div>
-            <h3>Apply for Job</h3>
+            < h1 className='text-center text-3xl font-bold my-6'>Apply for Job</h1>
+            <div className='text-center'></div>
+            <form onSubmit={handleApply} className='max-w-sm mx-auto p-6 bg-white shadow-md rounded-lg space-y-4 my-12 fieldset'>
+                <fieldset className="fieldset border-base-200 rounded-box w-xs border p-4">
+                    <label className="label">LinkedIn Link</label>
+                    <input type="url" className="input" name='linkedIn' placeholder="Linked in profile link" />
+                </fieldset>
+                <fieldset className="fieldset border-base-200 rounded-box w-xs border p-4">
+                    <label className="label">GitHub Link</label>
+                    <input type="url" className="input" name='gitHub' placeholder="GitHub profile link" />
+                </fieldset>
+                <fieldset className="fieldset border-base-200 rounded-box w-xs border p-4">
+                    <label className="label">Resume Link</label>
+                    <input type="url" className="input" name='resume' placeholder="Resume link" />
+                </fieldset>
+                <div className='px-4'>
+                    <input type="submit" value="Apply" className='btn btn-outline btn-primary btn-block ' />
+                </div>
+            </form>
         </div>
     );
 };
